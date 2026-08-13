b4a1de7fb3689ff2b73570458b4e8a5b1ee243f5
dwww9292-collab <dwww9292@gmail.com>
2026-08-13 10:53:33 +0900

    히어로 슬라이드 2번을 odongdo-2로 교체

--- src/data/demoImages.ts
+++ src/data/demoImages.ts
@@ -49,8 +49,9 @@ function group(name: keyof typeof DEMO_IMAGE_GROUPS): string[] {
  */
 export const DEMO_HERO_IMAGES: string[] = [
   "/images/hero1.png",
-  DEMO_IMAGES["odongdo-1"],
+  DEMO_IMAGES["odongdo-2"],
   "/images/island1.png",
+  DEMO_IMAGES["odongdo-1"],
   DEMO_IMAGES["odongdo-3"],
   DEMO_IMAGES["odongdo-4"],
   DEMO_IMAGES["odongdo-5"],
